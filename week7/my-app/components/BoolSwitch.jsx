f529d8d7173511e3012c6ce1ff7af9d81624ff21
madiha-sumbal <133386903+madiha-sumbal@users.noreply.github.com>
2024-10-19 15:43:26 +0500

    Delete week7/my-app directory

--- week7/my-app/components/BoolSwitch.jsx
+++
@@ -1,9 +0,0 @@
-import React , {useState} from 'react'
-import { Switch } from 'react-native'
-export default function BoolSwitch() {
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(!isEnabled);
-  return (
-   <Switch value={isEnabled} onValueChange={toggleSwitch}   ></Switch>
-  )
-}
