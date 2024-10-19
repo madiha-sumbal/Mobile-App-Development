a73641b9734fa720e0c32628a85d563774da68ad
madiha-sumbal <133386903+madiha-sumbal@users.noreply.github.com>
2024-10-19 15:55:27 +0500

    Delete week7/my-app directory

--- week7/my-app/components/BarStatus.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { StatusBar } from 'react-native'
-export default function BarStatus() {
-  return (
-    <>
-    <StatusBar barStyle="light-content" hidden={false} backgroundColor = "#00BCD4" ></StatusBar>
-    </>
-  )
-}
